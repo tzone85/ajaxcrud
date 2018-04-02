e8e42651bf9a8c80e772bfe50c5381f68c8b0649
thando <tzone85@gmail.com>
2018-04-02 16:20:42 +0200

    added tasks as the new vue file as well as prepped the view for the tasks vue file

---
+++ resources/assets/js/components/Tasks.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">My Tasks</div>
+
+                    <div class="panel-body">
+                        <div class="input-group">
+                            <input type="text" class="form-control">
+                            <span class="input-group-btn">
+                            <button class="btn btn-success">
+
+                            </button>
+                            </span>
+                        </div>
+
+                        <div class="task-list">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <div class="checkbox"><label><input type="checkbox"></label>Task 1</div>
+                                </li>
+                                <li>
+                                    <div class="checkbox"><label><input type="checkbox"></label>Task 2</div>
+                                </li>
+                                <li>
+                                    <div class="checkbox"><label><input type="checkbox"></label>Task 3</div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
